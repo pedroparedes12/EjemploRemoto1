8c5fdd6327ef028adc6c5cd7a4b221bf8659fc5b
usuario <usuario@DESKTOP-2MA9OEK>
2026-04-09 14:52:47 +0200

    Prueba de repositorio remoto

--- Ejercicio16/src/paquete1/Principal.java
+++ Ejercicio16/src/paquete1/Principal.java
@@ -6,6 +6,10 @@ public class Principal {
 		
 		System.out.println ("Estamos haciendo control de versiones con Git");
 		System.out.println ("Segunda prueba de control de versiones con Git");
+		System.out.println ("Estamos haciendo control de versiones con Git");
+		System.out.println ("Segunda prueba de control de versiones con Git");
+		System.out.println ("Estamos haciendo control de versiones con Git");
+		System.out.println ("Segunda prueba de control de versiones con Git");
 		
 
 	}
