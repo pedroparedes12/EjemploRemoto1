6924120326832269e0514883dc91eca9d9034be1
usuario <usuario@DESKTOP-2MA9OEK>
2026-04-13 11:09:35 +0200

    Hemos borrado varias lineas

--- Ejercicio16/src/paquete1/Principal.java
+++ Ejercicio16/src/paquete1/Principal.java
@@ -5,11 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		
 		System.out.println ("Estamos haciendo control de versiones con Git");
-		System.out.println ("Segunda prueba de control de versiones con Git");
-		System.out.println ("Estamos haciendo control de versiones con Git");
-		System.out.println ("Segunda prueba de control de versiones con Git");
-		System.out.println ("Estamos haciendo control de versiones con Git");
-		System.out.println ("Segunda prueba de control de versiones con Git");
+		
 		
 
 	}
