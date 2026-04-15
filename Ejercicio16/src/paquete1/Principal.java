82e8f5deb861db9e95f4b6a9a2c79e0bcd081012
usuario <usuario@DESKTOP-2MA9OEK>
2026-04-15 09:07:28 +0200

    Prueba con la rama master

--- Ejercicio16/src/paquete1/Principal.java
+++ Ejercicio16/src/paquete1/Principal.java
@@ -4,8 +4,8 @@ public class Principal {
 
 	public static void main(String[] args) {
 		
+		System.out.println ("Estamos haciendo control de versiones con Git, con la rama principal");
 		System.out.println ("Estamos haciendo control de versiones con Git");
-		
 		
 
 	}
